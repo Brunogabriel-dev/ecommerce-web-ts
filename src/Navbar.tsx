411c4ab8225239784c735b88ac055507d6724309
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-16 13:11:40 -0300

    Inclusão de texto

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,5 +1,12 @@
 const Navbar = () => {
-  return <div className="container hidden lg:block">Navbar</div>
+  return <div className="container hidden lg:block">
+    <div className="flex justify-between items-center pt-8">
+      <h1 className="text-4x1 font-medium">Logo</h1>
+      <div className="relative w-full max-w-[500px]">
+        <input className="bg-[#f2f3f5]" type="text" placeholder="Search Product..."/>
+      </div>
+    </div>
+  </div>
 };
 
 export default Navbar
