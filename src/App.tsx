959637991315f62f36a18feddb7921a6a1833d33
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-16 13:01:49 -0300

    Inclusão de texto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import Navbar from "./Navbar"
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <main>
+     <Navbar/>
+    </main>
   )
 }
 
---
+++ src/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="container hidden lg:block">Navbar</div>
+};
+
+export default Navbar
