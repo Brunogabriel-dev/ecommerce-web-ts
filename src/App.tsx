feb89da28f551b73b476b3f14215d23ef7ab0376
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-19 09:42:44 -0300

    Inclusão de texto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Category from "./components/Category"
+import FeatureSectionFruits from "./components/FeatureSectionFruits"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <main>
-     <Navbar/>
-     <Hero/>
-     <Category/>
+     <Navbar />
+     <Hero />
+     <Category />
+     <FeatureSectionFruits />
     </main>
   )
 }
--- src/components/FeatureSectionFruits.tsx
+++ src/components/FeatureSectionFruits.tsx
@@ -3,8 +3,17 @@ const FeatureSectionFruits = () => {
     <div className="container pt-16">
       <div className="lg:flex justify-between items-center">
         <div>
-          <h3></h3>
+          <h3 className="font-medium text-2xl">Fruits & Vegetables</h3>
+          <p className="text-gray-600 mt-2">Buy farm fresh fruits and vegetables online at the best prices</p>
         </div>
+
+
+        <div className="space-x-4 mt-8 lg:mt-0">
+         <button className="feature_btn">Fruits</button>
+          <button>Vegetables</button>
+          <button>Vegetables</button>
+        </div>
+
       </div>
     </div>
   )
