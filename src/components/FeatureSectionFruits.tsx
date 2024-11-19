d3747de4130202c234e64124157f34f1a9991375
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-19 09:46:43 -0300

    Inclusão de texto

--- src/components/FeatureSectionFruits.tsx
+++ src/components/FeatureSectionFruits.tsx
@@ -10,8 +10,8 @@ const FeatureSectionFruits = () => {
 
         <div className="space-x-4 mt-8 lg:mt-0">
          <button className="feature_btn">Fruits</button>
-          <button>Vegetables</button>
-          <button>Vegetables</button>
+          <button className="text-gray-600 hover:text-accent">Vegetables</button>
+          <button className="text-gray-600 hover:text-accent">Bread & Bakery</button>
         </div>
 
       </div>
