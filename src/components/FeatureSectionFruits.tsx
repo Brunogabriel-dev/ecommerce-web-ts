7d470a4708d779bf6af3ebbc8449849e1f6d2cbb
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-19 09:28:23 -0300

    Inclusão de texto

---
+++ src/components/FeatureSectionFruits.tsx
@@ -0,0 +1,13 @@
+const FeatureSectionFruits = () => {
+  return (
+    <div className="container pt-16">
+      <div className="lg:flex justify-between items-center">
+        <div>
+          <h3></h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeatureSectionFruits
