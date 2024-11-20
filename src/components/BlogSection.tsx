d6b616aba0a73e148996a7d33908dd8b84702e0b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-20 11:50:07 -0300

    Inclusão de texto

---
+++ src/components/BlogSection.tsx
@@ -0,0 +1,28 @@
+const data = [
+  {
+    img: "/post__1.webp",
+    title: "Healthy food Healthy Life",
+    date: "Nov 27, 2024",
+    commrnt: 8,
+  },
+  {
+    img: "/post__2.webp",
+    title: "Healthy Food Healthy Life",
+    date: "Nov 28, 2024",
+    comment: 1,
+  },
+  {
+    img: "/post__3.webp",
+    title: "Healthy Food Healthy Life",
+    date: "Nov 30, 2024",
+    comment: "6",
+  }
+];
+
+const BlogSection = () => {
+  return (
+    <div>BlogSection</div>
+  )
+}
+
+export default BlogSection
