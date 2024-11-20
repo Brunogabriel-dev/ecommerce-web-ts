9ea0e16756c71ca3fbb5f7a07a300122fa85b6ac
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-20 11:11:49 -0300

    Inclusão de texto

--- src/components/FeatureSectionBreakFast.tsx
+++ src/components/FeatureSectionBreakFast.tsx
@@ -1,10 +1,10 @@
 import ProductCard from "./ProductCard";
 
 const data = [
-  { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
-  { id: 1, img: "product__2.webp", name: "Crunchy Crisps", price: "$300" },
-  { id: 2, img: "product__3.webp", name: "Jewel Cranberries", price: "$200" },
-  { id: 3, img: "product__4.webp", name: "Almond organic", price: "$100" },
+    { id: 0, img: "product__1.webp", name: "Dried Mango", price: "$500" },
+    { id: 1, img: "product__2.webp", name: "Crunchy Crisps", price: "$300" },
+    { id: 2, img: "product__3.webp", name: "Jewel Cranberries", price: "$200" },
+    { id: 3, img: "product__4.webp", name: "Almond organic", price: "$100" },
 ];
 
 const FeatureSectionBreakFast = () => {
@@ -19,20 +19,16 @@ const FeatureSectionBreakFast = () => {
         </div>
 
         <div className="space-x-4 mt-8 lg:mt-0">
-         <button className="feature_btn">Fruits</button>
-          <button className="text-gray-600 hover:text-accent">
-            Vegetables
-            </button>
-          <button className="text-gray-600 hover:text-accent">
-            Bread & Bakery
-            </button>
+          <button className="feature_btn">Eggs & Dairy</button>
+          <button className="text-gray-600 hover:text-accent">Pizza</button>
+          <button className="text-gray-600 hover:text-accent">Snacks</button>
         </div>
-      </div>
+        </div>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 pt-8 gap-2">
         <img 
         className="w-full h-full object-cover" 
-        src="/feature__1.webp" 
+        src="/feature__2.webp" 
         alt="banner" />
       
       {data.map((el)=> (
