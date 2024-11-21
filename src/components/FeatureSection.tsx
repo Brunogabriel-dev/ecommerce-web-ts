15e83ed6749df380a113a5e209fc7b162b0a3488
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-21 15:59:34 -0300

    Inclusão de texto

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,12 +1,38 @@
+import {
+  LiaShippingFastSolid,
+  LiaMoneyBillWaveSolid,
+  LiaGiftSolid,
+} from "react-icons/lia";
 
+import { FiPhoneCall } from "react-icons/fi";
+
+const data = [
+  {
+    title: "Free Shipping",
+    icon: <LiaShippingFastSolid />,
+  }
+  {
+    title: "Best Price Guarantee",
+    icon: <LiaMoneyBillWaveSolid />,
+  }
+  {
+    title: "Free Curbside Pickuo",
+    icon: <LiaGiftSolid />,
+  }
+  {
+    title: "Support 24h",
+    icon: <FiPhoneCall />,
+  }
+]
 
 const FeatureSection = () => {
   return (
     <div className="container pt-16">
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {data.map}
       </div>
     </div>
   )
 }
 
-export default FeatureSection
+export default FeatureSection;
