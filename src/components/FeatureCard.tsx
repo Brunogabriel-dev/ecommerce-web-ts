93b6acebefc39865fafbb789995551bd3cce57aa
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-21 16:44:23 -0300

    Inclusão de tetxo

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -6,12 +6,12 @@ interface propsType {
 const FeatureCard: React.FC<propsType> = ({ title, icon}) => {
   return (
     <div className="flex items-center gap-4">
-      <div className="bg-gray-300 w-[50px] text-accentDark text-[24px] grid place-items-center rounded-full">
+      <div className="bg-gray-300 w-[50px] h-[50px] text-accentDark text-[24px] grid place-items-center rounded-full">
         {icon}
       </div>
       <div>
         <h3 className="text-accent font-medium text-xl">{title}</h3>
-        <p className="text-gray-500 text-[14px]">Loren, ipsum dolor.</p>
+        <p className="text-gray-500 text-[-14px]">Loren, ipsum dolor.</p>
       </div>
     </div>
   )
