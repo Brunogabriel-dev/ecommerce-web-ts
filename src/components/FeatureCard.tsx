fca53f72579130d753434f147be21cc17cbb182f
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-21 16:05:50 -0300

    Inclusão de texto

---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,13 @@
+interface propsType {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const FeatureCard: React.FC<propsType> = ({ title, icon}) => {
+  return (
+    <div className=""></div>
+  )
+}
+
+export default FeatureCard
+
